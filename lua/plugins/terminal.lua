10955ca4bc0456b4d4ad0fbeb241e05f3f3dc226
Bac Truong Van <=>
2024-07-29 09:16:00 +0700

    add lazygit terminal

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -5,7 +5,20 @@ return {
       direction = "horizontal",
     })
 
-    vim.keymap.set('n', '<leader>1', ':1ToggleTerm<CR>')
-    vim.keymap.set('n', '<leader>2', ':2ToggleTerm<CR>')
+    local opts = {
+      noremap = true,
+      silent = true,
+    }
+
+    vim.keymap.set('n', '<leader>1', ':1ToggleTerm<CR>', opts)
+    vim.keymap.set('n', '<leader>2', ':2ToggleTerm<CR>', opts)
+
+    local Terminal = require('toggleterm.terminal').Terminal
+    local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float" })
+    function _lazygit_toggle()
+      lazygit:toggle()
+    end
+
+    vim.keymap.set("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", opts)
   end
 }
