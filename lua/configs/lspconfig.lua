5274745c278577263e923d0d0b09a63b9dd05577
Bac Truong Van <bactruongvan17@gmail.com>
2024-08-28 17:40:05 +0700

    add phpactor

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "gopls", "tsserver", "jdtls" }
+local servers = { "html", "cssls", "gopls", "tsserver", "jdtls", "phpactor" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
