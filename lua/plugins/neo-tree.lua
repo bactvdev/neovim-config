f44f65c541ca15bb2c1e02794a32263a7542ecc1
Bac Truong Van <bactruongvan17@gmail.com>
2025-02-14 22:38:05 +0700

    add open in file explorer

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -19,6 +19,30 @@ return function(use)
 						folder_empty = "󰜌",
 					},
 				},
+				window = {
+					mappings = {
+						["O"] = function(state)
+							local node = state.tree:get_node()
+							local path = node:get_id()
+							local open_cmd
+
+							if vim.fn.has("win32") == 1 then
+								open_cmd = "explorer"
+							elseif vim.fn.has("mac") == 1 then
+								open_cmd = "open"
+							else
+								open_cmd = "xdg-open"
+							end
+
+							-- Nếu là file, mở thư mục chứa file
+							if vim.fn.isdirectory(path) == 0 then
+								path = vim.fn.fnamemodify(path, ":h")
+							end
+
+							vim.fn.jobstart({ open_cmd, path }, { detach = true })
+						end,
+					},
+				},
 			})
 		end,
 	})
